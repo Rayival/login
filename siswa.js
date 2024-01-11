1330ca24285734e315fe82a91e96276d9e41a96f
Rayival Dede Saputra <113332359+Rayival@users.noreply.github.com>
2024-01-11 17:25:19 +0700

    Delete siswa.js

--- siswa.js
+++
@@ -1,7 +0,0 @@
-const namaSiswa  = [" ", " ", "", " ", " "];
-let siswa = "";
-
-for (let i = 0; i < namaSiswa.length; i++) {
-  siswa += namaSiswa[i] + "<br>"
-}
-document.getElementById("Siswa").innerHTML = siswa;
